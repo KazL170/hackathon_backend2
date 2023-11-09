29c582844986a437ad47f75d18bd4cc5dd483f56
KazL <kazph1707@gmail.com>
2023-11-09 18:55:38 +0900

    revise

--- hackathon/main.go
+++ hackathon/main.go
@@ -43,14 +43,15 @@ func init() {
 	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
 
 	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
-	db, err := sql.Open("mysql", connStr)
+	_db, err := sql.Open("mysql", connStr)
 	if err != nil {
 		log.Fatalf("fail: sql.Open, %v\n", err)
 	}
 	// ①-3
-	if err := db.Ping(); err != nil {
+	if err := _db.Ping(); err != nil {
 		log.Fatalf("fail: _db.Ping, %v\n", err)
 	}
+	db = _db
 
 }
 
